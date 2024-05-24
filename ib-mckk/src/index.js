164d7ec9a23576a7b0048cecd82383a49fc5bc9b
Adam Jeffery <adamjeff143@gmail.com>
2024-05-24 16:30:01 +0100

    test

--- ib-mckk/src/index.js
+++ ib-mckk/src/index.js
@@ -1,25 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import {Has as Router} from 'react-router-dom';
 import Top from './Components/topContact/topContact';
 import { NavBar } from './Components/Nav/navBar';
 import { Footer } from './Components/Footer/footer';
-
-import Home from './Components/Home/Home';
-import Bog from './Components/Bog/Bog';
-import Admin from './Components/Admin/Admin';
-import Teacher from './Components/Teacher/Teacher';
-import IbCoco from './Components/IbCoco/IbCoco';
-import Counselling from './Components/Counselling/Counselling';
-import Admission from './Components/Admission/Admission';
-import Contact from './Components/Contact/Contact';
-import Achievement from './Components/Achievement/Achievement';
-import Student from './Components/Student/Student';
-import Facilities from './Components/Facilities/Facilities';
-import Activities from './Components/Activities/Activities';
-import Graduation from './Components/Graduation/Graduation';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -30,26 +17,11 @@ root.render(
           <Top />
           <NavBar />
         </div>
-
+        
         <div className='index-main'>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/Bog" component={Bog} />
-            <Route path="/Admin" component={Admin} />
-            <Route path="/Teacher" component={Teacher} />
-            <Route path="/IbCoco" component={IbCoco} />
-            <Route path="/Counselling" component={Counselling} />
-            <Route path="/Admission" component={Admission} />
-            <Route path="/Contact" component={Contact} />
-            <Route path="/Achievement" component={Achievement} />
-            <Route path="/Student" component={Student} />
-            <Route path="/Facilities" component={Facilities} />
-            <Route path="/Activities" component={Activities} />
-            <Route path="/Graduation" component={Graduation} />
-            {/* Add more routes as needed */}
-          </Switch>
+          <App />
         </div>
-
+        
         <div className='index-footer'>
           <Footer />
         </div>
